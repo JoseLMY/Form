e09157f36d9af279dc04ed29401e6334382156c3
Jose Luis <jose.lmy2004@gmail.com>
2023-07-25 18:13:15 -0500

    Formulario funcional.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 const $form = document.querySelector("#form")
 $form.addEventListener("submit", handleSubmit)
-function handleSubmit(e) {
-    e.preventDefault()
 
+async function handleSubmit(e) {
+    e.preventDefault()
     const form = new FormData(this)
-    console.log(form.get("name"));
+    const response = await fetch(this.action, {
+        method: this.method,
+        body: form,
+        headers: {
+            "Accept": "application/json"
+        }
+    })
+    if (response.ok) {
+        this.reset()
+        alert("Gracias por contactarme. te escribiré pronto")
+    }
 }
